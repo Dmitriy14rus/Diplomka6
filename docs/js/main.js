a26617c0eb290085a14521bd2053dfd08c2c52e7
Dmitriy14rus <129680732+Dmitriy14rus@users.noreply.github.com>
2023-10-24 19:39:22 +0900

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.typesStairs__swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.typesStairs__btn-next',
+        prevEl: '.typesStairs__btn-prev',
+    },
+});
